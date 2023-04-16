699f465bc416cefd887df3a931efbcfd7e3d355e
dupe <njdupe@gmail.com>
2023-04-16 19:58:12 -0400

    feat: implementing encoding huffman algorithm

--- algorithms-archive-python/huffman-encoding/huff_zip.py
+++ algorithms-archive-python/huffman-encoding/huff_zip.py
@@ -6,36 +6,31 @@ import argparse
 
 from huffman_encoder.encoder import HuffmanEncoder
 
+
 def parse_args() -> argparse.Namespace:
     """ Parses the command line arguments """
-    parser = argparse.ArgumentParser(description='Zip and unzip files using Huffman coding.')
+    parser = argparse.ArgumentParser(
+        description='Zip and unzip files using Huffman coding.')
     parser.add_argument('input_path', help='path to the input file')
     parser.add_argument('output_path', help='path to the output file')
-    parser.add_argument('-d', '--decode', action='store_true', help='decode the input file')
+    parser.add_argument('-d', '--decode', action='store_true',
+                        help='decode the input file')
     return parser.parse_args()
 
-def decode(input_path: str, output_path: str) -> None: 
+
+def decode(input_path: str, output_path: str) -> None:
     """ Decodes the input file and writes the output to the output file """
     encoder = HuffmanEncoder()
+    file = encoder.fit_decoder(input_path)
+    encoder.decode(file, output_path)
 
-    with open(input_path, 'r') as input_file:
-        input_lines = input_file.readlines()
-        encoder.fit_decoder(input_lines)
-        decoded = encoder.decode(input_lines)
-     
-    with open(output_path, 'w') as output_file:
-        output_file.writelines(decoded)
 
 def encode(input_path: str, output_path: str) -> None:
     """ Encodes the input file and writes the output to the output file """
     encoder = HuffmanEncoder()
-    with open(input_path, 'r') as input_file:
-        input_lines = input_file.readlines()
-        encoder.fit_encoder(input_lines)
-        encoded = encoder.encode(input_lines)
+    encoder.fit_encoder(input_path)
+    encoder.encode(input_path, output_path)
 
-    with open(output_path, 'w') as output_file:
-        output_file.writelines(encoder.encode(encoded))
 
 def main() -> None:
     args = parse_args()
@@ -44,6 +39,7 @@ def main() -> None:
         decode(args.input_path, args.output_path)
     else:
         encode(args.input_path, args.output_path)
+
 
 if __name__ == '__main__':
     main()
--- algorithms-archive-python/huffman-encoding/huffman/encoder.py
+++ algorithms-archive-python/huffman-encoding/huffman/encoder.py
@@ -2,54 +2,106 @@
 This module implements a Huffman encoder and decoder for compressing
 and decompressing files.
 """
+
+import heapq
+import typing
+
+
+class HuffmanNode:
+
+    def __init__(self,
+                 character: typing.Optional[str],
+                 count: int,
+                 left: typing.Optional['HuffmanNode'] = None,
+                 right: typing.Optional['HuffmanNode'] = None) -> None:
+        self.character = character
+        self.count = count
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f'HuffmanNode({self.character}, {self.count})'
+
+    def __lt__(self, other: 'HuffmanNode') -> bool:
+        return self.count < other.count
+
+    def __eq__(self, other: 'HuffmanNode') -> bool:
+        return self.count == other.count
+
+    def __gt__(self, other: 'HuffmanNode') -> bool:
+        return self.count > other.count
+
+
 class HuffmanEncoder:
 
     def __init__(self) -> None:
         """
         Initializes a new Huffman encoder.
         """
-        self._encoding_tree: list[tuple[str, int]] = []
+        self._encoding_tree: typing.Optional[HuffmanNode] = None
         self._encoding_table: dict[str, str] = {}
         self._character_counts: dict[str, int] = {}
 
-    def _count_characters(self, input: list[str]) -> None:
-        """
-        Counts the number of times each character appears in the given input.
-        """
-        for line in input:
-            for character in line:
-                self._character_counts[character] = self._character_counts.get(character, 0) + 1
+    def _count_characters(self, input: str) -> None:
+        """ Counts the number of times each character appears in the given input. """
+        for character in input:
+            self._character_counts[character] = self._character_counts.get(
+                character, 0) + 1
 
     def _build_encoding_tree(self) -> None:
         """
         Builds the encoding tree from the character counts.
         """
-        pass
+        heap = []
+        for character, count in self._character_counts.items():
+            heapq.heappush(heap, HuffmanNode(character, count))
+
+        while len(heap) > 1:
+            left = heapq.heappop(heap)
+            right = heapq.heappop(heap)
+            heapq.heappush(heap, HuffmanNode(
+                None, left.count + right.count, left, right))
+
+        self._encoding_tree = heap[0]
 
     def _build_encoding_table(self) -> None:
         """
         Builds the encoding table from the encoding tree.
         """
-        pass
 
-    def fit_encoder(self, input: list[str]) -> None:
+        stack = []
+        stack.append((self._encoding_tree, ''))
+        while len(stack) > 0:
+            node, code = stack.pop()
+            if node.character is not None:
+                self._encoding_table[node.character] = code
+            else:
+                stack.append((node.left, code + '0'))
+                stack.append((node.right, code + '1'))
+
+    def fit_encoder(self, input_file: str) -> None:
         """
         Fits the encoder to the given input.
         """
-        self._count_characters(input)
+        with open(input_file, 'r') as file:
+            for line in file:
+                self._count_characters(line)
         self._build_encoding_tree()
         self._build_encoding_table()
 
-    def fit_decoder(self, input: list[str]) -> None:
+    def fit_decoder(self, input_encoded_file: str) -> typing.BinaryIO:
         """
-        Fits the decoder to the given input.
+        Fits the decoder to the given input. Returns the file object opened
+        in binary mode. The rest of the file can be decoded using the now
+        fit Huffman decoder.
+
+        The file object returned by this method should be closed by the
+        caller.
         """
         pass
 
-    def encode(self, input: list[str]) -> list[str]:
+    def encode(self, input_file: str, destination: str) -> None:
         print('Encoding...')
-        return [':)']
 
-    def decode(self, input: list[str]) -> list[str]:
+    def decode(self, encoded_file: typing.BinaryIO, destination: str) -> None:
         print('Decoding...')
-        return [':)']
