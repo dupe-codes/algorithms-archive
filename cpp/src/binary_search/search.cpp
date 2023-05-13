1e0f9b64a0038502061a26baf212f8a203b376e8
dupe <njdupe@gmail.com>
2023-05-13 13:29:50 -0400

    feat: test precommit hook again

--- cpp/src/binary_search/search.cpp
+++ cpp/src/binary_search/search.cpp
@@ -1,23 +1,20 @@
 #include <algorithms_archive/binary_search/search.h>
 
 namespace binary_search {
-    
 
-
-
-    int search(int* arr, int size, int target) {
-        int left = 0;
-        int right = size - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else if (arr[mid] > target) {
-                right = mid - 1;
-            }
+int search(int *arr, int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
         }
-        return -1;
     }
+    return -1;
 }
+} // namespace binary_search
