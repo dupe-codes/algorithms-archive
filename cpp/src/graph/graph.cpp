a33ea700200d260397cb0ef476d64eaec31a400a
dupe <njdupe@gmail.com>
2023-05-29 16:23:01 -0400

    fix: update initializer list matrix constructor

--- cpp/src/graph/graph.cpp
+++ cpp/src/graph/graph.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <memory>
+#include <set>
 
 #include "algorithms_archive/graph/graph.h"
 
@@ -8,8 +9,16 @@ using namespace algorithms_archive::graph;
 AdjacencyMatrixGraph::AdjacencyMatrixGraph(std::size_t size)
     : size_{size}, matrix_{std::unique_ptr<int[]>{new int[size * size]}} {}
 
-AdjacencyMatrixGraph::AdjacencyMatrixGraph(std::initializer_list<Edge> edges)
-    : AdjacencyMatrixGraph{edges.size()} {
+AdjacencyMatrixGraph::AdjacencyMatrixGraph(std::initializer_list<Edge> edges) {
+    // count number of unique nodes in the given edges list
+    std::set<int> nodes;
+    for (const auto &edge : edges) {
+        nodes.insert(edge.from.id);
+        nodes.insert(edge.to.id);
+    }
+
+    size_ = nodes.size();
+    matrix_ = std::unique_ptr<int[]>{new int[size_ * size_]};
     for (const auto &edge : edges) {
         addEdge(edge);
     }
